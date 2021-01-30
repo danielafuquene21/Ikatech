3183b1a66b69f162116d53131eb76ec7e73a07c9
danielafuquene21 <danielafuquene21@gmail.com>
2021-01-30 11:01:51 -0500

    Eliminacion codigo comentado en la clase DataBase

--- app/src/main/java/com/ikatech/businessObject/DataBase.java
+++ app/src/main/java/com/ikatech/businessObject/DataBase.java
@@ -29,25 +29,7 @@ public class DataBase extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-    /*public void editarUsuario(User u, User user){
-        SQLiteDatabase db = getWritableDatabase();
-        try {
-            if(db!=null) {
-                db.execSQL("update Nits set N_IDE = '" + u.getId() + "', C_EMP = '" + u.getCodEmpresa() + "', " +
-                        "NOM = '" + u.getNombrePersonaJuridica() + "', IDE = '" + u.getTipoId() + "', DIR = '" + u.getDireccion() + "', " +
-                        "TEL = '" + u.getTelefono() + "', AA = '" + u.getApartadoAereo() + "'," +
-                        " EST = '" + u.getEstado() + "' , NOM1 = '" + u.getPrimerNombre() + "', NOM2 = '" + u.getSegundoNombre() + "'," +
-                        " APE1 = '" + u.getPriemrApellido() + "', APE2 = '" + u.getSegundoApellido() + "', " +
-                        "DIR_ELECT = '" + u.getCorreo() + "', DIG = '" + u.getDigitoVerificacion() + "'," +
-                        "  TIPO_NIT = '" + u.getTipoNit() + "', TIP_CONTRIB = '" + u.getTipoContribuyente() + "' , " +
-                        "TIP_TERC = '" + u.getTipoTercero() + "', FECHA = '" + u.getFecha() + "' where N_IDE = '" + user.getId() + "'");
-            }
-        }catch (Exception e) {
-        } finally {
-            if (db != null) db.close();
-        }
 
-    }*/
     public void login (User u){
         SQLiteDatabase db = getWritableDatabase();
         try{
@@ -294,79 +276,5 @@ public class DataBase extends SQLiteOpenHelper {
         return cant;
     }
 
-    /*public List<User> mostrarUsuarios(){
-        SQLiteDatabase db = getReadableDatabase();
-        List<User> usuarios = new ArrayList<>();
-        try{
-            Cursor cursor =db.rawQuery("SELECT N_IDE, NOM, IDE , DIG FROM Nits ", null);
-
-            User u = new User();
-            if(cursor.moveToFirst()){
-                do{
-                    u = new User();
-                    u.setId(cursor.getInt(cursor.getColumnIndex("N_IDE")));
-                    u.setNombrePersonaJuridica(cursor.getString(cursor.getColumnIndex("NOM")));
-                    u.setTipoNit(cursor.getString(cursor.getColumnIndex("IDE")));
-                    u.setDigitoVerificacion(cursor.getString(cursor.getColumnIndex("DIG")));
-                    usuarios.add(u);
-                }while (cursor.moveToNext());
-            }
-            if (cursor != null) cursor.close();
-            if (db != null) db.close();
-        }catch (Exception e) {
-        } finally {
-            if (db != null) db.close();
-        }
-        return usuarios;
-    }
-    public User cargarUsuario(String codigo){
-        SQLiteDatabase db = getReadableDatabase();
-        List<User> usuarios = new ArrayList<>();
-        User u = new User();
-        try{
-            Cursor cursor =db.rawQuery("SELECT * FROM Nits where N_IDE ='"+codigo+"'", null);
-
-            if(cursor.moveToFirst()){
-                do{
-                    u = new User();
-                    u.setId(cursor.getInt(cursor.getColumnIndex("N_IDE")));
-                    u.setNombrePersonaJuridica(cursor.getString(cursor.getColumnIndex("NOM")));
-                    u.setTipoNit(cursor.getString(cursor.getColumnIndex("IDE")));
-                    u.setDigitoVerificacion(cursor.getString(cursor.getColumnIndex("DIG")));
-                    u.setDireccion(cursor.getString(cursor.getColumnIndex("DIR")));
-                    u.setTelefono(cursor.getString(cursor.getColumnIndex("TEL")));
-                    u.setApartadoAereo(cursor.getString(cursor.getColumnIndex("AA")));
-                    u.setEstado(cursor.getString(cursor.getColumnIndex("EST")));
-                    u.setPrimerNombre(cursor.getString(cursor.getColumnIndex("NOM1")));
-                    u.setSegundoNombre(cursor.getString(cursor.getColumnIndex("NOM2")));
-                    u.setPriemrApellido(cursor.getString(cursor.getColumnIndex("APE1")));
-                    u.setSegundoApellido(cursor.getString(cursor.getColumnIndex("APE2")));
-                    u.setTipoId(cursor.getString(cursor.getColumnIndex("TIPO_NIT")));
-                    u.setTipoContribuyente(cursor.getString(cursor.getColumnIndex("TIP_CONTRIB")));
-                    u.setTipoTercero(cursor.getString(cursor.getColumnIndex("TIP_TERC")));
-                    u.setFecha(cursor.getString(cursor.getColumnIndex("FECHA")));
-
-                }while (cursor.moveToNext());
-
-            }
-            if (cursor != null) cursor.close();
-            if (db != null) db.close();
-        }catch (Exception e) {
-        } finally {
-            if (db != null) db.close();
-        }
-        return u;
-    }
-
-    public void eliminarUsuario(String codigo){
-
-        SQLiteDatabase db = getWritableDatabase();
-        try{
-            db.execSQL("DELETE FROM "+TABLA_NOMBRE+ " WHERE N_IDE = '"+codigo+"' ");
-        }catch (Exception e) {
-        } finally {
-            if (db != null) db.close();
-        }
-    }*/
 
 }
